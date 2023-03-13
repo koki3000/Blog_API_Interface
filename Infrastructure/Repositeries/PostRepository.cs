5032631433c7658f6c0991d22e7aac96081b1006
Konrad P <kokiziom@gmail.com>
2023-03-13 11:41:29 +0100

    Add project files.

---
+++ Infrastructure/Repositeries/PostRepository.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositeries
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly BloggerContext _context;
+
+        public PostRepository(BloggerContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Post> GetAll()
+        {
+            return _context.Posts;
+        }
+
+        public Post GetById(int id)
+        {
+            return _context.Posts.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Post Add(Post post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+            return post;
+        }
+
+        public void Update(Post post)
+        {
+            _context.Posts.Update(post);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Post post)
+        {
+            _context.Posts.Remove(post);
+            _context.SaveChanges();
+        }
+
+    }
+}
